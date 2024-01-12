910ab49d4d7aca2f5c2b6b6dc45dd80521642942
teddi90 <slobodyanoleg5@gmail.com>
2024-01-12 16:54:15 +0200

    start project

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!--   navbar   -->
+        <Navbar/>
+
+        <!-- Sidebar -->
+        <Sidebar/>
+
+        <slot/>
+
+
+    </div>
+</template>
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Navbar from "@/Pages/Admin/Components/Navbar.vue";
+import Sidebar from "@/Pages/Admin/Components/Sidebar.vue";
+
+
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
