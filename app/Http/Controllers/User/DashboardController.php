4c0c62975d16a2f2261ecc7528bee487fde9f615
teddi90 <slobodyanoleg5@gmail.com>
2024-02-14 19:38:24 +0200

    fixed purchase list in dashboard page -only show user purcheses

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $orders = Order::with('order_items.product.brand', 'order_items.product.category')->get();
-
+        $userId = Auth::id();
+        $orders = Order::where('created_by', $userId)
+            ->with('order_items.product.brand', 'order_items.product.category')
+            ->get();
         return Inertia::render('User/Dashboard',['orders'=>$orders]);
     }
 }
--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -7,55 +7,59 @@ defineProps({
 
 <template>
     <UserLayout>
-
-
-        <div class="relative max-w-screen-xl py-24 mx-auto overflow-x-auto">
-            <table v-for="order in orders" :key="order.id" class="mb-7 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Order ID #{{order.id}}
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Brand
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Category
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Price
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="item in order.order_items" :key="item.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{item.product.title}}
-                    </th>
-                    <td class="px-6 py-4">
-                        {{item.product.brand.name}}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{item.product.category.name}}
-                    </td>
-                    <td class="px-6 py-4">
-                        ${{item.product.price}}
-                    </td>
-                </tr>
-                <tr class=" bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <th class=" px-5 py-2 uppercase">Total price:</th>
-                    <th></th>
-                    <th></th>
-
-
-                    <th class=" px-5 py-2">
-                        ${{order.total_price}}
-                    </th>
-                </tr>
-                </tbody>
-            </table>
+        <div class="container mx-auto pt-24">
+            <div class="relative overflow-x-auto">
+                <table v-for="order in orders" :key="order.id" class="mb-20 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 rounded-s-lg w-1/5">
+                            Order ID #{{order.id}}
+                        </th>
+                        <th scope="col" class="px-6 py-3 w-1/5">
+                            Brand
+                        </th>
+                        <th scope="col" class="px-6 py-3 w-1/5">
+                            Category
+                        </th>
+                        <th scope="col" class="px-6 py-3 w-1/5">
+                            Quantity
+                        </th>
+                        <th scope="col" class="px-6 py-3 rounded-e-lg w-1/5">
+                            Price
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="item in order.order_items" :key="item.id" class="bg-white dark:bg-gray-800">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-1/5">
+                            {{item.product.title}}
+                        </th>
+                        <th class="px-6 py-4 w-1/5">
+                            {{item.product.brand.name}}
+                        </th>
+                        <th class="px-6 py-4 w-1/5">
+                            {{item.product.category.name}}
+                        </th>
+                        <th class="px-6 py-4 w-1/5">
+                            {{item.quantity}}
+                        </th>
+                        <td class="px-6 py-4 w-1/5">
+                            ${{item.unit_price}}
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tfoot>
+                    <tr class="font-semibold text-gray-900 dark:text-white bg-gray-50 ">
+                        <th scope="row" class="px-6 py-2 text-base rounded-s-lg">Date:</th>
+                        <td class="px-6 py-2">{{order.updated_at.split('T')[0]}}  </td>
+                        <td class="px-6 py-2">{{order.updated_at.split('T')[1].split('.')[0]}}</td>
+                        <td class="px-6 py-2 text-base">Total:</td>
+                        <td class="px-6 py-2 rounded-e-lg text-base"> ${{order.total_price}}</td>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
         </div>
-
     </UserLayout>
 </template>
 
